c156f9e8c140bc5c8742067a686ca28cade0dad8
Bradley Bain <brad_bain@live.com>
2014-10-17 22:37:05 -0500

    Update version to 1.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 scripts = ['bin/snapception']
 
 setup(name='snapception',
-	  version='1.0',
+	  version='1.1.1',
 	  author='Bradley Bain',
 	  license='MIT',
 	  packages=['snapception'],
@@ -13,6 +13,6 @@ setup(name='snapception',
 	  	'mitmproxy'
 	  ],
 	  include_package_data=True,
-	  entry_points={'console_scripts' : ['snapception=snapception.command_line:main']}
+	  entry_points={'console_scripts' : ['snapception=snapception.command_line:launch']}
 	  )
 	 
